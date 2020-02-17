94aa5b9d7d9d1e6743e0b7962dc46cab3ef499cb
Steven Ross <ross.steven@me.com>
2020-02-17 16:41:11 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,25 +1,15 @@
 def valid_move?(board, index)
-  def position_taken?(array, ind)
-    if array[ind] == " " || array[ind] == "" || array[ind] == nil
-      return false
-    else
-      return true
-    end
-  end
-
-  def on_board?(num)
-    if num.between?(0, 8) == true
+  if index.between(1, 9)
+    if !position_taken(board, index)
       return true
     else
       return false
     end
   end
 
-  if (position_taken?(board, index)) == false && (on_board?(index) == true)
-    return true
-  else
-    return false
-  end
+  index.between(0, 8) && !position_taken(board, index)
+end
 
-
+def position_taken?(board, index)
+  board[index] != " "
 end
